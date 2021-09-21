2623ab20f036181e50f74bd0b19d645d1831b831
Vitor Diorio <vitor.diorio@yahoo.com.br>
2021-09-21 00:33:08 -0300

    Criado a sessão de projetos ultilizando o api do github

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
 function transformButton(menu) {
   menu.classList.toggle("change");
 }
+
+function createProjectCard({ title, html_url}) {
+  const name = title.split('] ')[1]
+  const link = html_url;
+  const newCard = document.createElement('div');
+  newCard.innerText = name;
+  newCard.className = 'small-card'
+  newCard.addEventListener('click', function() {
+    location.href = link;
+}, false);
+  document.querySelector('#project-cards').appendChild(newCard);
+}
+
+function getVitorPullRequests() {
+  return fetch('https://api.github.com/search/issues?q=state%3Aopen+author%3Avdiorio+type%3Apr')
+  .then((response) => response.json())
+  .then((object) => object.items.forEach((project) =>{
+    createProjectCard(project);
+  }));
+}
+
+function createLoadingElement(){
+  
+}
+
+function deleteLoadingElement(){
+  
+}
+
+async function loadProjects() {
+  createLoadingElement();
+  await getVitorPullRequests();
+  deleteLoadingElement();
+}
+
+window.onload = () => {
+  loadProjects();
+}
